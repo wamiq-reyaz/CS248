2c20960ca7e642c86189694f139ce60d96ec59aa
wamreyaz <wamiq.para@kaust.edu.sa>
2017-09-04 13:34:23 +0300

    main.cpp added basic loading capability

--- P1.2/main.cpp
+++ P1.2/main.cpp
@@ -1,10 +1,15 @@
 #include"vec.h"
 #include"mat.h"
+
+#include"mesh.h"
+
 #include<stdio.h>
 #include<stdlib.h>
 
+
 int main(int argc, char** argv) {
 	// TODO -- test every vector and matrix function. Submit your code with tests in here.
-
+	mesh_t aa;
+	aa.load(argv[1]);
 	return 0;
 }
