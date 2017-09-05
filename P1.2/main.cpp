64a357753f0a23b9fcfbcdb0ffcae7e3afaf65b8
wamreyaz <wamiq.para@kaust.edu.sa>
2017-09-05 12:48:36 +0300

    Add debug to main

--- P1.2/main.cpp
+++ P1.2/main.cpp
@@ -11,5 +11,7 @@ int main(int argc, char** argv) {
 	// TODO -- test every vector and matrix function. Submit your code with tests in here.
 	mesh_t aa;
 	aa.load(argv[1]);
+	aa.dbgdump();
+	std::cout << aa.check_mesh() << std::endl;
 	return 0;
 }
