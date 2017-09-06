686c5d5706ac3517e8cc67eec66a1ae1420bbf2e
wamreyaz <wamiq.para@kaust.edu.sa>
2017-09-06 16:30:20 +0300

    check boundaries

--- P1.2/main.cpp
+++ P1.2/main.cpp
@@ -12,6 +12,17 @@ int main(int argc, char** argv) {
 	mesh_t aa;
 	aa.load(argv[1]);
 	aa.dbgdump();
+	mesh_t::halfedge_iterator he_it;
+
+	/* This tests for the 
+	*/
+	for(he_it = aa.halfedge_begin(); he_it != aa.halfedge_end(); he_it++){
+		halfedge_t edge = *he_it;
+		if(edge.on_border()){
+			std::cout << edge.id << " on boundary " << std::endl;
+		}
+	}
+
 	std::cout << aa.check_mesh() << std::endl;
 	return 0;
 }
