de2e0f03e0356cda256f2bed15a95f3a9114ea43
wamreyaz <wamiq.para@kaust.edu.sa>
2017-09-10 11:54:23 +0300

    Cleaned up main.cpp

--- P1.2/main.cpp
+++ P1.2/main.cpp
@@ -3,51 +3,29 @@
 
 #include"mesh.h"
 
+#include"catch.hpp"
+
 #include<stdio.h>
 #include<stdlib.h>
 
 
+std::string ParseArgs(int argc, char**argv){
+	if(argc != 2){
+		std::cout << "Usage: ./main <Model>";
+		exit(EXIT_FAILURE);
+	}
+	else return std::string(argv[1]);
+}
+
 int main(int argc, char** argv) {
-	// TODO -- test every vector and matrix function. Submit your code with tests in here.
-	mesh_t aa;
-	aa.load(argv[1]);
-	aa.dbgdump();
-	
+	// TODO dump the mesh to a file.
+	mesh_t test_mesh;
+	test_mesh.load(ParseArgs(argc, argv));
+	test_mesh.dbgdump();
 
-	/* This tests for the 
-	*/
-	#ifdef _DEBUG
-	mesh_t::halfedge_iterator he_it;
-	for(he_it = aa.halfedge_begin(); he_it != aa.halfedge_end(); he_it++){
-		halfedge_t edge = *he_it;
-		if(edge.on_border()){
-			// std::cout << edge.id << " on boundary " << std::endl;
-		}
-	}
-	#endif
+	// check mesh
+	std::cout << test_mesh.check_mesh() << std::endl;
 
-	#ifdef _DEBUG
-	mesh_t::vertex_iterator v_it;
-	for(v_it = aa.vertex_begin(); v_it != aa.vertex_end(); v_it++){
-		// vertex_t v = *v_it;
-		// std::cout << " Degree: " << v.degree() << std::endl;
-
-		// if(v.on_border()){
-		// 	std::cout << "on border: " << v.id << std::endl;
-		// }
-	}
-
-	mesh_t::facet_iterator facet_it;
-	for(facet_it = aa.facet_begin(); facet_it != aa.facet_end(); facet_it++){
-		facet_t f = *facet_it;
-			// std::cout << f.id << " Degree: " << f.degree() << std::endl;
-			//std::cout << f.id << " On Border: " << f.on_border() << std::endl;
-	}
-
-
-	#endif
-
-	// std::cout << aa.check_mesh() << std::endl;
-	aa.clear();
+	test_mesh.clear();
 	return 0;
 }
