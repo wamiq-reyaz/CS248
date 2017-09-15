baa9e70fea86c4b94e89fd72dbf817791430ee12
wamreyaz <wamiq.para@kaust.edu.sa>
2017-09-15 10:54:24 +0300

    All tests completed for quat.h

--- P2.1/test.cpp
+++ P2.1/test.cpp
@@ -5,6 +5,16 @@
 int main(int argc, char** argv){
     quat<float> q1(1,0,1,0), q2(1,0.5,0.5,0.75);
     vecf v1;
-    std::cout << q1*q1 << " " << v1 << std::endl;
+    std::cout << "Self multipliation and printing "<< q1*q2 << " " << v1 << std::endl;
+    std::cout << "conjugate of 1,0,1,0 " << q1.conjugate() << std::endl;
+    std::cout << "conjugate of 1, .5, .5, .75 " << q2.conjugate() << std::endl;
+    q1 *= q2;
+    std::cout << "operator *= with above quarternions "<< q1 << " " << std::endl;
+
+    std::cout << "inverse of q1 " << q1.inverse() << std::endl;
+    std::cout << "q1 * q1.inverse " << q1*q1.inverse() << std::endl; 
+
+    std::cout << "inverse of q2 " << q2.inverse() << std::endl;
+    std::cout << "q2 * q2.inverse " << q2*q2.inverse() << std::endl; 
     return 0;
 }
